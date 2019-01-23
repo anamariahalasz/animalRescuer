bd2c4c35aef8794c7c42feeb368ac56462f379ca
anamariahalasz <ana.mariahalasz@yahoo.com>
2019-01-23 17:14:04 +0200

    If-else statements

--- src/main/java/org/fasttrackit/Adopter.java
+++ src/main/java/org/fasttrackit/Adopter.java
@@ -23,13 +23,21 @@ public class Adopter extends Person {
         System.out.println(animal.getName() + " Level of hunger: " + animal.getLevelOfHunger());
         animal.setLevelOfHunger(animal.getLevelOfHunger() - 1);
         System.out.println(animal.getName() + " New level of hunger: " + animal.getLevelOfHunger());
+        if(animal.getNameOfFavoriteFood().equals(foodForAnimals.getName()))
+            animal.setHappinessLevel(animal.getHappinessLevel() + 1);
     }
 
     public void activityWithAnimal (Animal animal,RecreationalActivity recreationalActivity) {
         System.out.println(this.getName() + " " + recreationalActivity.getName() + " with " + animal.getName());
 
         System.out.println(animal.getName() + " Happiness level: " + animal.getHappinessLevel());
-        animal.setHappinessLevel(animal.getHappinessLevel() + 1);
+        if (animal.getNameOfFavoriteRecreationalActivity().equals(recreationalActivity.getName()) )
+            animal.setHappinessLevel(animal.getHappinessLevel() + 2);
+        else
+            animal.setHappinessLevel(animal.getHappinessLevel() + 1);
+
         System.out.println(animal.getName() + " New happiness level: " + animal.getHappinessLevel());
+
+
     }
 }
--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -12,13 +12,17 @@ public class App
     public static void main( String[] args )
     {
 
-        FoodForAnimals firstFoodForAnimals = new FoodForAnimals("chicken meat", 1.25, 5, LocalDate.of(2019, 4, 23), true);
+        FoodForAnimals dogFood = new FoodForAnimals("chicken meat", 1.25, 5, LocalDate.of(2019, 4, 23), true);
+
+        FoodForAnimals catFood = new FoodForAnimals("Whiskas", 1.75, 7, LocalDate.of(2019, 5, 12), true);
 
         RecreationalActivity firstRecreationalActivity = new RecreationalActivity("running");
 
         Animal dog = new Dog("Oscar", 1, 8);
         dog.setLevelOfHunger(5);
         dog.setHappinessLevel(7);
+        dog.setNameOfFavoriteFood("bones");
+        dog.setNameOfFavoriteRecreationalActivity("running");
 
         Adopter firstAdopter = new Adopter("Jake", 29, "cop", 1000);
 
@@ -26,12 +30,16 @@ public class App
 
         Game firstGame = new Game(firstAdopter,dog,firstVeterinaryDoctor);
 
-        firstAdopter.feedTheAnimal(dog, firstFoodForAnimals);
+        firstAdopter.feedTheAnimal(dog, dogFood);
         firstAdopter.activityWithAnimal(dog,firstRecreationalActivity);
 
         Animal cat = new Animal ("Tom", 2, 9);
         cat.setLevelOfHunger(3);
         cat.setHappinessLevel(8);
+        cat.setNameOfFavoriteFood("Whiskas");
+        cat.setNameOfFavoriteRecreationalActivity("sleeping");
+        firstAdopter.feedTheAnimal(cat, catFood);
+        firstAdopter.activityWithAnimal(cat, firstRecreationalActivity);
 
         dog.mood();
         cat.mood();
